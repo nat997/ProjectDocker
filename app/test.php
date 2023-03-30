dc46408236b9075e6a1e89908031873ba0e990ec
nat997 <67456959+nat997@users.noreply.github.com>
2023-03-30 14:34:57 +0200

    Update test.php

--- app/test.php
+++ app/test.php
@@ -1,6 +1,6 @@
 <?php
-$file_contents = file_get_contents('/app/index.php');
-$pattern = '/<h1> hello world <\/h1>/';
+$file_contents = file_get_contents('index.php');
+$pattern = '/<h1>Hello world<\/h1>/';
 if (preg_match($pattern, $file_contents)) {
     echo "La balise <h1> hello world </h1> est présente dans le fichier index.php.";
 } else {
